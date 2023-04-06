7cbef0b29f8f126a1a04971d6f66023abe34eb99
leessii <ursa.lesnik@gmail.com>
2023-04-06 12:32:54 +0200

    server splited in routes

---
+++ server/routes/employees.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const EmployeeModel = require("../db/employee.model");
+
+router.get("/", async (req, res) => {
+    const employees = await EmployeeModel.find().sort({ created: "desc" });
+    return res.json(employees);
+});
+  
+router.get("/:id", async (req, res) => {
+    const employee = await EmployeeModel.findById(req.params.id);
+    return res.json(employee);
+});
+  
+router.post("/", async (req, res, next) => {
+    const employee = req.body;
+
+    try {
+        const saved = await EmployeeModel.create(employee);
+        return res.json(saved);
+    } catch (err) {
+        return next(err);
+    }
+});
+  
+router.patch("/:id", async (req, res, next) => {
+    try {
+        const employee = await EmployeeModel.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: { ...req.body } },
+        { new: true }
+        );
+        return res.json(employee);
+    } catch (err) {
+        return next(err);
+    }
+});
+  
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const employee = await EmployeeModel.findById(req.params.id);
+        const deleted = await employee.delete();
+        return res.json(deleted);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
